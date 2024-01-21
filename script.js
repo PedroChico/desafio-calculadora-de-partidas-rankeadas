711d141a74f44c7c793ef4d9b94457d05d3e37c8
PedroChico <pedro.joel.chico@gmail.com>
2024-01-20 21:15:59 -0300

    Conclusão do projeto

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-funct
+function heroi(vitorias, derrotas) {
+    let saldo = vitorias - derrotas;
+    return saldo;
+}
+
+let saldoHeroi = heroi(350, 100);
+console.log("O saldo do Herói é de " + saldoHeroi + " vitórias");
+
+switch (true) {
+    case saldoHeroi < 10:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Ferro");
+        break;
+    case saldoHeroi > 10 && saldoHeroi <= 20:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Bronze");
+        break;
+    case saldoHeroi > 20 && saldoHeroi <= 50:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Prata");
+        break;
+    case saldoHeroi > 50 && saldoHeroi <= 80:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Ouro");
+        break;
+    case saldoHeroi > 80 && saldoHeroi <= 90:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Diamante");
+        break;
+    case saldoHeroi > 90 && saldoHeroi <= 100:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Lendário");
+        break;
+    case saldoHeroi > 100:
+        console.log("O Herói tem saldo de " + saldoHeroi + " e está no nível de Imortal");
+        break;
+    }
